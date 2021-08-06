49870a9c97d6eb48d3aa4183467616679080ecd7
mbevko <mbevko@gmail.com>
2021-08-06 09:46:26 -0400

    ran build

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -107,7 +107,6 @@ export default function InputForm() {
                     <div>
                         <img src='https://images.unsplash.com/photo-1528721071427-cab7de8e8050?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'/>
                     </div>
-
                 </div>
                 <div className="social_icons">
                     {socialCard.twitter
